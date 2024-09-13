bc02b94c28b2b4679ae5e62a925a06b6398b8979
xufu2025 <xufu2025@gmail.com>
2024-09-13 15:32:33 +0100

    add update

--- WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/Controls/HeaderControl.xaml.cs
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/Controls/HeaderControl.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WiredBrainCoffee.CustomersApp
+namespace WiredBrainCoffee.CustomersApp.Controls
 {
   /// <summary>
-  /// Interaction logic for MainWindow.xaml
+  /// Interaction logic for HeaderControl.xaml
   /// </summary>
-  public partial class MainWindow : Window
+  public partial class HeaderControl : UserControl
   {
-    public MainWindow()
+    public HeaderControl()
     {
       InitializeComponent();
     }
